fd1c74b33402a20ac0e9f749ca0b1a188f226998
Aidan <Aidan@DESKTOP-RSEUQAO>
2020-10-07 20:29:54 -0700

    first commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,34 @@
+/**
+* The AddingMachine program is a basic calculator
+* that will display the output using toString.
+*
+* @author  Aidan Hoppe
+* @version 1.0
+* @since   2020-October-7 
+*/
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
